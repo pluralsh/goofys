0a112c8b434b9e959017d70a53943013eaea951e
DavidSpek <vanderspek.david@gmail.com>
2021-09-09 19:14:39 +0200

    cleanup startup script

--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-[[ "$UID" -ne "0" ]] && MOUNT_ACCESS="allow_other" || MOUNT_ACCESS="nonempty"
-
 goofys -f ${ENDPOINT:+--endpoint $ENDPOINT} ${REGION:+--region $REGION} $EXTRA_OPTS --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE --uid $UID --gid $GID -o $MOUNT_ACCESS $BUCKET $MOUNT_DIR
